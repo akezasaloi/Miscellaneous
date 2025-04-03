47e0f72c4e6e362b8f735bbf29ea659babefc0cd
Akeza Saloi <akezasaloi@gmail.com>
2025-04-03 12:02:05 +0300

    practiced pip questions

---
+++ index.py
@@ -0,0 +1,14 @@
+# n = 3
+# a= [0 for x in range(n)]
+# i = 1
+# while (i< n):
+# a[i] = input("Enter the first name of students which start with a vowel:" )
+# i = i+1
+# b = [0 for x in range(n)]
+# j = 1
+# b[j] = input("Enter the first name of students which start with a consonant:" )
+# j = j+1
+# print(len(a))
+# print(len(a))
+
+
