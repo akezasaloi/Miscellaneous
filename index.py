ebce663ff2310520227f5972cb53752df10364f2
Akeza Saloi <akezasaloi@gmail.com>
2025-04-03 17:08:28 +0300

    manipulated strings in python

--- index.py
+++ index.py
@@ -1,14 +1,65 @@
-# n = 3
-# a= [0 for x in range(n)]
-# i = 1
-# while (i< n):
-# a[i] = input("Enter the first name of students which start with a vowel:" )
-# i = i+1
-# b = [0 for x in range(n)]
-# j = 1
-# b[j] = input("Enter the first name of students which start with a consonant:" )
-# j = j+1
-# print(len(a))
-# print(len(a))
-
+# string = "extravaganza"
+# >>> newString = string[-4]
+# >>> print(newString)
+# a
+# >>> newString = string[-4:]
+# >>> print(newString)
+# anza
+# >>> #vdsvsd
+# >>> newstring = concat(str1,added
+# KeyboardInterrupt
+# >>> str = "the quick fox jumped over the lazy dog"
+# >>> x = "the quick fox jumped over the lazy dog"
+# >>> B = x.Lower().count("the)
+#   File "<stdin>", line 1
+#     B = x.Lower().count("the)
+#                         ^
+# SyntaxError: unterminated string literal (detected at line 1)
+# >>> B = x.lower().count("the)
+#   File "<stdin>", line 1
+#     B = x.lower().count("the)
+#                         ^
+# SyntaxError: unterminated string literal (detected at line 1)
+# >>> B = x.lower()
+# >>> c = B.count("the")
+# >>> print(c)
+# 2
+# >>> str = "hello world, hello Javascript"
+# >>> str1 = str.lower()
+# >>> str2 = str.split()
+# >>> print (str2)
+# ['hello', 'world,', 'hello', 'Javascript']
+# >>> print (str2.count("hello"))
+# 2
+# >>> str3 = str2.count("hello")
+# >>> print ('hello appears f{str2} times')
+# hello appears f{str2} times
+# >>> printf('hello appears %str2 times')
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'printf' is not defined. Did you mean: 'print'?
+# >>> print (f'hello appears {str2} times')
+# hello appears ['hello', 'world,', 'hello', 'Javascript'] times
+# >>> print (f'hello appears {str3} times')
+# hello appears 2 times
+# >>> stri = "greeting"
+# >>> print(str.find("t"))
+# 28
+# >>> print(stri.find("t"))
+# 4
+# >>> print(stri[2:])
+# eeting
+# >>> print(stri[:-2])
+# greeti
+# >>> 
+# >>> string = " alice    "
+# >>> newString = string.strip().lower()
+# >>> resultString = newString[0].upper() + newString[1:]
+# >>> print(resultString)
+# Alice
+# >>> newString = string.trim().lower()
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# AttributeError: 'str' object has no attribute 'trim'. Did you mean: 'strip'?
+# >>> 
 
