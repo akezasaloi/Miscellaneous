02165604991c348a47a54386e10590230e19efb8
Akeza Saloi <akezasaloi@gmail.com>
2025-04-03 17:04:57 +0300

    manipulated strings in kotlin

---
+++ index.kt
@@ -0,0 +1,31 @@
+//fun main(){
+//    var string = "    alice "
+//    var newString = string.trim().lowercase()
+//    var resultString = newString[0].uppercaseChar() + newString.substring(1)
+//    println(resultString)
+//    var str = "extravaganza"
+//
+//    println(str.substring(str.length-4))
+// counts("The brown quick fox jumped over the lazy dog")
+//findIndex("greeting")
+//}
+//fun counts(str:String) {
+//var firstcount = 0
+// var secondcount =0
+// var string1 = str.lowercase()
+//var string2 = string1.split(" ")
+// var counted = string2.forEach { x ->
+//  if (x == "the") {
+//   firstcount++
+//  }
+//  if (x == "brown"){
+//   secondcount++
+//  }
+// }
+// println(" the appears $firstcount times and brown appears $secondcount times")
+//}
+//fun findIndex(str:String){
+// println(str.indexOf("t"))
+// var str2 = str.substring(0,str.length-2)
+// println(str2)
+//}
